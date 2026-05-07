94ffe3a49412a356e3974d393fa9584bba3b3b66
RobertoOteri <rob.oteri@stud.uniroma3.it>
2026-05-07 16:20:28 +0200

    Create e Inizializzate le classi AnimaleRepositort, AnimaleService e AnimaleController, creati i folder admin e animali all'interno di templates e animali all'interno di admin

---
+++ src/main/java/it/uniroma3/siw/repository/AnimaleRepository.java
@@ -0,0 +1,9 @@
+package it.uniroma3.siw.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.model.Animale;
+
+public interface AnimaleRepository extends CrudRepository<Animale, Long> {
+
+}
---
+++ src/main/java/it/uniroma3/siw/service/AnimaleService.java
@@ -0,0 +1,34 @@
+package it.uniroma3.siw.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.siw.repository.AnimaleRepository;
+import it.uniroma3.siw.model.*;
+
+@Service
+public class AnimaleService {
+	
+	private AnimaleRepository animaleRepository;
+	
+	public AnimaleService(AnimaleRepository animaleRepository) {
+		this.animaleRepository = animaleRepository;
+	}
+	
+	public Animale save(Animale animale) {
+		return this.animaleRepository.save(animale);
+	}
+	
+	public List<Animale> findAll(){
+		return (List<Animale>) this.animaleRepository.findAll();
+	}
+	
+	public Animale findById(Long id) {
+		return animaleRepository.findById(id).get();
+	}
+	
+	public void deleteById(Long id) {
+		animaleRepository.deleteById(id);
+	}
+}
