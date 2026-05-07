c567ab280e3ea63f1286f9b22515202c3506ccd7
RobertoOteri <rob.oteri@stud.uniroma3.it>
2026-05-07 12:26:29 +0200

    Aggiunte entità Animale e Volontario

--- src/main/java/it/uniroma3/siw/model/Animale.java
+++ src/main/java/it/uniroma3/siw/model/Animale.java
@@ -1,6 +1,177 @@
 package it.uniroma3.siw.model;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class Animale {
-	private int gay; // prova 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private Specie specie;
+	
+	@Column(nullable = false)
+	private String razza;
+	
+	private LocalDate dataNascita;
+	
+	@Column(nullable = false)
+	private LocalDate dataArrivo;
+	
+	private String descrizione;
+	
+	@ManyToOne(cascade = {CascadeType.PERSIST})
+	private Volontario volontario;
+	
+	@ManyToOne
+	private Area area;
+	
+	@OneToOne
+	private CartellaClinica cartellaClinica;
+	
+	@OneToMany(mappedBy = "animale", cascade = {CascadeType.REMOVE})
+	private List<RichiestaAdozione> richiesteDiAdozione;
+	
+	// Constructors //
+	
+	public Animale() {}
+	
+	public Animale(String nome, Specie specie, String razza, LocalDate dataNascita, LocalDate DataArrivo, String descrizione) {
+
+		this.nome = nome;
+		this.specie = specie;
+		this.razza = razza;
+		this.dataNascita = dataNascita;
+		this.dataArrivo = dataArrivo;
+		this.descrizione = descrizione;
+	}
+	
+	// Getters And Setters //
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Specie getSpecie() {
+		return specie;
+	}
+
+	public void setSpecie(Specie specie) {
+		this.specie = specie;
+	}
+
+	public String getRazza() {
+		return razza;
+	}
+
+	public void setRazza(String razza) {
+		this.razza = razza;
+	}
+
+	public LocalDate getDataNascita() {
+		return dataNascita;
+	}
+
+	public void setDataNascita(LocalDate dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+
+	public LocalDate getDataArrivo() {
+		return dataArrivo;
+	}
+
+	public void setDataArrivo(LocalDate dataArrivo) {
+		this.dataArrivo = dataArrivo;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public Volontario getVolontario() {
+		return volontario;
+	}
+
+	public void setVolontario(Volontario volontario) {
+		this.volontario = volontario;
+	}
+
+	public Area getArea() {
+		return area;
+	}
+
+	public void setArea(Area area) {
+		this.area = area;
+	}
+
+	public CartellaClinica getCartellaClinica() {
+		return cartellaClinica;
+	}
+
+	public void setCartellaClinica(CartellaClinica cartellaClinica) {
+		this.cartellaClinica = cartellaClinica;
+	}
+
+	public List<RichiestaAdozione> getRichiesteDiAdozione() {
+		return richiesteDiAdozione;
+	}
+
+	public void setRichiesteDiAdozione(List<RichiestaAdozione> richiesteDiAdozione) {
+		this.richiesteDiAdozione = richiesteDiAdozione;
+	}
+
+	// HashCode And Equals //
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(dataNascita, nome, razza, specie);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Animale other = (Animale) obj;
+		return Objects.equals(dataNascita, other.dataNascita) && Objects.equals(nome, other.nome)
+				&& Objects.equals(razza, other.razza) && specie == other.specie;
+	}
+
 
 }
---
+++ src/main/java/it/uniroma3/siw/model/Volontario.java
@@ -0,0 +1,128 @@
+package it.uniroma3.siw.model;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Volontario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String cognome;
+
+	private String codiceFiscale;
+	private String specializzazione;
+	
+	@Column(nullable = false)
+	private LocalDate dataDiNascita;
+	
+	@OneToMany(mappedBy = "volontario")
+	private List<Animale> listaAnimali;
+	
+	// Constructors //
+	
+	public Volontario() {}
+	
+	public Volontario(String nome, String cognome, String codiceFiscale, String specializzazione, LocalDate dataDiNascita) {
+
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceFiscale = codiceFiscale;
+		this.specializzazione = specializzazione;
+		this.dataDiNascita = dataDiNascita;
+	}
+	
+	// Getters And Setters //
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+
+	public String getSpecializzazione() {
+		return specializzazione;
+	}
+
+	public void setSpecializzazione(String specializzazione) {
+		this.specializzazione = specializzazione;
+	}
+
+	public LocalDate getDataDiNascita() {
+		return dataDiNascita;
+	}
+
+	public void setDataDiNascita(LocalDate dataDiNascita) {
+		this.dataDiNascita = dataDiNascita;
+	}
+
+	public List<Animale> getListaAnimali() {
+		return listaAnimali;
+	}
+
+	public void setListaAnimali(List<Animale> listaAnimali) {
+		this.listaAnimali = listaAnimali;
+	}
+
+	// HashCode And Equals //
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(codiceFiscale, cognome, dataDiNascita, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Volontario other = (Volontario) obj;
+		return Objects.equals(codiceFiscale, other.codiceFiscale) && Objects.equals(cognome, other.cognome)
+				&& Objects.equals(dataDiNascita, other.dataDiNascita) && Objects.equals(nome, other.nome);
+	}
+	
+	
+}
