dbef1e76a89a049dcb416133f7d75ec626babbff
valelynx <val.difusco@stud.uniroma3.it>
2026-06-03 15:16:54 +0200

    import.sql

--- src/main/java/it/uniroma3/siw/model/Animale.java
+++ src/main/java/it/uniroma3/siw/model/Animale.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -28,6 +30,7 @@ public class Animale {
 	private String nome;
 	
 	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
 	private Specie specie;
 	
 	private String razza;
